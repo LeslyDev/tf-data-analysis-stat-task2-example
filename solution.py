d8b616d9fdccbc19eec43b948237d5523bc05933
LeslyDev <54473665+LeslyDev@users.noreply.github.com>
2023-04-07 19:52:59 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = p
+    alpha = 1 - p
     loc = x.mean()
-    return (loc + 0.5 - erlang.ppf(alpha / 2, len(x), loc=0, scale=1 / len(x))) / 14, \
-           (loc + 0.5 - erlang.ppf(1 - alpha / 2, len(x), loc=0, scale=1 / len(x))) / 14
+    return (loc - 0.5 + erlang.ppf(alpha / 2, len(x), loc=0, scale=1 / len(x))) / 14, \
+           (loc - 0.5 + erlang.ppf(1 - alpha / 2, len(x), loc=0, scale=1 / len(x))) / 14
